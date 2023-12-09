182de9f6258f1d945d016b43e21fc4f5b63e18e2
Nicole Mihal <nicolemihal@Nicoles-MacBook-Air.local>
2023-12-08 22:24:38 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import {
+  Route,
+  Routes,
+} from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import LeftBar from "./components/leftBar/LeftBar";
+import RightBar from "./components/rightBar/RightBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import "./style.scss";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
+import { AuthContext } from "./context/authContext";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const {currentUser} = useContext(AuthContext);
+
+  const { darkMode } = useContext(DarkModeContext);
+
+    return (
+      <div className={`theme-${darkMode ? "dark" : "light"}`}>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <div style={{ flex: 6 }}>
+          <Routes>
+           <Route path="/" element={<Home/>}/>
+           <Route path="/profile/:id" element={<Profile/>}/>
+           <Route path="/login" element={<Login/>}/>
+           </Routes>
+          </div>
+          <RightBar />
+        </div>
+      </div>
+    );
 }
 
 export default App;
